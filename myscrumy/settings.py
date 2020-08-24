d4ab470a6d451147373768a4018578a942f1be53
OGUNSOLA USMAN ADEBAYO <ogunsoladebayo@gmail.com>
2020-08-24 00:59:01 +0000

    Update settings.py

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-2b1=o6n(ey$i=0)2iqzxei(cat_0mx^nchor)+(7ou9m2#*9e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.243.43.134', '172.31.41.48']
+ALLOWED_HOSTS = ['3.136.161.16']
 
 
 # Application definition
