675739ee5d9a9ce81b269395e79cfff3fc5b4bd0
ogunsoladebayo <ogunsoladebayo@gmail.com>
2020-09-09 06:28:38 +0100

    lab 12

--- ogunsoladebayoscrumy/urls.py
+++ ogunsoladebayoscrumy/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.get_grading_parameters),
-    path('movegoal/<int:goal_id>', views.move_goal)
+    path('movegoal/<int:goal_id>', views.move_goal),
+    path('addgoal/', views.add_goal),
+    path('home/', views.home)
 ]
--- ogunsoladebayoscrumy/views.py
+++ ogunsoladebayoscrumy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User, ScrumyGoals, ScrumyHistory, GoalStatus
+from random import randint
 
 # Create your views here.
 
@@ -12,3 +13,17 @@ def get_grading_parameters(request):
 def move_goal(request, goal_id):
     goal = ScrumyGoals.objects.get(goal_id=goal_id)
     return HttpResponse(goal)
+
+
+def add_goal(request):
+    status = GoalStatus.objects.get(status_name='Weekly Goal')
+    user = User.objects.get(username='louis')
+    ScrumyGoals.objects.create(goal_name='Keep Learning Django', goal_id=randint(
+        1000, 9999), created_by='Louis', moved_by='Louis', goal_status=status, user=user)
+    return HttpResponse('Done')
+
+
+def home(request):
+    goals = ScrumyGoals.objects.filter(goal_name='Keep Learning Django')
+    goal = ', '.join([goal.goal_name for goal in goals])
+    return(HttpResponse(goal))
