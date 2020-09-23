aad7efec69dc3295067a6f328923b6ed8dadea95
Ogunsola <ogunsoladebayo@gmail.com>
2020-09-23 04:17:35 +0100

    Implementing websocket server

--- websocket/urls.py
+++ websocket/urls.py
@@ -4,7 +4,7 @@ from django.urls.conf import include
 from . import views
 app_name = 'websocket'
 urlpatterns = [
-    path('test', views.test, name='test'),
-    path('connect', views.connect, name='connect'),
-    path('disconnect', views.disconnect, name='disconnect'),
+    path('test/', views.test, name='test'),
+    path('connect/', views.connect, name='connect'),
+    path('disconnect/', views.disconnect, name='disconnect'),
 ]
